ea2f0178354acdf17a95027f556c079ddf28107d
renatojobal <renatojobal@gmail.com>
2019-09-01 22:20:33 -0500

    Aun falta el logout

--- backend/login_app/views.py
+++ backend/login_app/views.py
@@ -2,16 +2,16 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import logout
 
 
 
 
 
 class Logout(APIView):
-    def get(self,request, format = None):
+    def get(self, request):
         request.user.auth_token.delete()
-        logout(request)
-        return Response(status = status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
 
 
 
