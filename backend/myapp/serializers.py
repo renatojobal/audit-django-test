e3cca10a5f029abd48a7b130ddcf8d940ff6fe99
renatojobal <renatojobal@gmail.com>
2019-09-07 14:19:46 -0500

    Creadno el serializador de rutas

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -5,10 +5,13 @@ from rest_framework.serializers import ModelSerializer
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = '__all__'
         
 
-
+class RoleSerializer(ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__'
 
 
 
