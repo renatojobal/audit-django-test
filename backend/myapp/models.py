73f33f5b4fa4817ffeab7812958312c8adf73473
renatojobal <renatojobal@gmail.com>
2019-09-07 14:09:35 -0500

    Creando los todos los modelos aún sin las relaciones

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+# La documemntación de los atributos se encuentra en el diagrama de entidades
+
 # Inheriting from AbstractUser
 class User(AbstractUser):
     """
 
     Attributes:
 
-    idUser equal to id from Django
+    idUser
     username provided by Django
     password provided by Django
     first_name provided by Django
@@ -17,7 +20,110 @@ class User(AbstractUser):
     birthday
     points
     """
-    
+    # * PK
+    idUser = models.AutoField(primary_key=True) 
+
+    # * Atributos relacionales
+
+    # * Otros atributos
     birthday = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=1, default='U', blank=True)
     points = models.IntegerField(blank=True, null=True)
+
+
+class City(models.User):
+    """
+    """
+    # * PK
+    idCity = models.AutoField(primary_key=True)
+
+    # * Atributos relacionales
+
+    # * Otros atributos
+    name = models.CharField(max_length=100)
+
+
+class TouristPoint(models.Model):
+    """
+    
+    """
+    # * PK
+    idTouristPoint = models.AutoField(primary_key=True)
+
+    # * Atributos relacionales
+
+    # * Otros atributos
+    name = models.CharField(max_length=100)
+    latituded = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+
+
+class Route(models.Model):
+    """
+    
+    """
+    # * PK
+    idRoute = models.AutoFieldd(primary_key=True)
+
+    # * Atributos relacionales
+
+    # * Otros atributos
+    name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
+
+
+class Restaurant(models.Model):
+    """
+    
+    """
+    # * PK
+    idRestaurant = models.AutoField(primary_key=True)
+    
+    # * Atributos relacionales
+
+    # * Otros atributos
+    name = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+
+
+class Prize(models.Model):
+    """
+    
+    """
+    # * PK
+    idPrize = models.AutoField(primary_key=True)
+
+    # * Atributos relacionales
+
+    # * Otros atributos
+    description = models.Tex
+
+
+class UserRole(models.Model):
+    """
+
+    """
+
+    # * PK
+    idUserRole = models.AutoField(primary_key=True)
+    
+    # * Atributos relacionales
+
+    # * Otros atributos
+    state = models.BooleanField(default=True)
+
+
+
+class Role(models.Model):
+    """
+    
+    """
+
+    # * PK
+    idRole = models.AutoField(primary_key=True)
+    
+    # * Atributos relacionales
+
+    # * Otros atributos
+    name = models.CharField(max_length=100)
