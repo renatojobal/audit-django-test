1bf67e8e85177af36f9a17c7fb9b6aac146521ae
renatojobal <renatojobal@gmail.com>
2019-09-12 23:00:59 -0500

    Registre los modelos con configuracions default

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
-from .models import User, Role
+from .models import City, TouristPoint, User, Role, Route, UserRole, Restaurant, Prize
 
-@admin.register(User)
-class User(admin.ModelAdmin):
+# @admin.register(User)
+# class User(admin.ModelAdmin):
+#     """
+#     Customizando la informacion que se vera en el admin site de django
+#     """
+#     list_display = ('username', 'first_name', 'last_name', 'email', 'gender', 'birthday', 'points', 'is_staff',)
+#     list_display_links = ('username', 'email',)
+#     list_editable = ('last_name', 'gender',)
 
-    list_display = ('username', 'first_name', 'last_name', 'email', 'gender', 'birthday', 'points', 'is_staff',)
-    list_display_links = ('username', 'email',)
-    list_editable = ('last_name', 'gender',)
+#     search_fields = ('username',)
 
-    search_fields = ('username',)
-
-    list_filter = ('points', 'is_staff', 'is_active',)
-
-@admin.register(Role)
-class Role(admin.ModelAdmin):
-
-    list_display =  ('name',)
-    list_display_links = ('name',)
-
-    search_fields = ('name',)
+#     list_filter = ('points', 'is_staff', 'is_active',)
 
 
+
+
+# * Registro de modelos con las funciones por defecto
+admin.site.register(City)
+admin.site.register(TouristPoint)
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Route)
+admin.site.register(UserRole)
+admin.site.register(Restaurant)
+admin.site.register(Prize)
