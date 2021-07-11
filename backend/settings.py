f18bfc5362068554a1bebbcc42780e055bb7e264
renatojobal <renatojobal@gmail.com>
2021-07-10 22:42:48 -0500

    asdasd

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -113,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
