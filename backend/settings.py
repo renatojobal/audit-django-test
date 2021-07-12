7d536e219f2ef36660c9dc04cb091f62dd7ac375
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2021-07-11 20:29:24 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PASSWORD': 'Thisisthepassword',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
